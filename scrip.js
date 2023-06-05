947d615d7742aeb6af9ec7855ad7ac90d72305cb
Julio <julio.cesar.2000x@gmail.com>
2023-06-05 19:29:46 -0300

    finalização das funções básicas do calendário

--- scrip.js
+++ scrip.js
@@ -1,27 +1,34 @@
 const currentDate = document.querySelector(".current-date"),
-daysTag = document.querySelector(".days"),
-prevNextIcon = document.querySelectorAll(".icons span");
+    daysTag = document.querySelector(".days"),
+    prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
 
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDate(), //last first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); //last date of month
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), //last first day of month
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), //last date of month
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
 
     for (let i = firstDayofMonth; i > 0; i--) {
-        liTag += `<li>${i}</li>`;
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-        liTag += `<li>${i}</li>`;
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
     }
 
-    currentDate.innerText = `${months[currMonth]} de ${currYear}`;
+    for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
 }
 
@@ -29,7 +36,15 @@ renderCalendar();
 
 prevNextIcon.forEach(icon => {
     icon.addEventListener("click", () => {
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        currMonth = icon.class === "bi bi-arrow-left-short" ? currMonth - 1 : currMonth + 1;
+
+        if(currMonth < 0 || currMonth > 11) {
+            date = new Date (currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
         renderCalendar();
     });
 });
